9013a482e3773064166be4a43fe71bc0f3860413
ole <olejorgen.naess@gmail.com>
2019-03-12 21:05:05 +0100

    Update of Index file

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
 <?php
 
-echo "dust";
+class User{
+    public function __construct()
+    {
+//        echo "Constructur called";
+    }
+
+
+    public function register(){
+        echo ' User Registered'. PHP_EOL;
+    }
+
+    public function login($username, $password){
+        $this->auth_user($username, $password);
+    }
+
+    public function auth_user($username, $password)
+    {
+        echo $username. ' is authenticated'.PHP_EOL;
+    }
+
+    public function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+//        echo 'Destructor called';
+    }
+}
+
+$User = new User;
+
+//$User->register();
+$User->login("Ole", "tester123");
